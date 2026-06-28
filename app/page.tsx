c7f6a12acf6678c0acdde4e65e5a60cd2c231372
Prime <primetechnologies02@gmail.com>
2026-06-28 06:24:28 +0100

    pushed the pae back

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Audience from "./layouts/Audience";
 import Focus from "./layouts/Focus";
 import Footer from "./layouts/Footer";
 import Hero from "./layouts/Hero";
-import Maintanance from "./layouts/Maintanance";
 import Navbar from "./layouts/Navbar";
 import Schedule from "./layouts/Schedule";
 import Speaker from "./layouts/Speaker";
@@ -12,15 +11,14 @@ import Speaker from "./layouts/Speaker";
 const page = () => {
   return (
     <main className="relative text-white min-h-screen">
-      {/* <Navbar />
+      <Navbar />
       <Hero />
       <About />
       <Focus />
       <Audience />
       <Speaker />
       <Schedule />
-      <Footer /> */}
-      <Maintanance />
+      <Footer />
     </main>
   );
 };
